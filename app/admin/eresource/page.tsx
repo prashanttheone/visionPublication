33168d01c7966d34c58529e22b3af8528c59888e
Prashant Kumar <Prashant.Yadav@adeptek.co.in>
2025-12-17 23:57:37 +0530

    eresoruce

---
+++ app/admin/eresource/page.tsx
@@ -0,0 +1,14 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import ManageEresources from '@/component/adminComponet/eresources/ManageEresources';
+
+export default async function EresourcePage() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('auth_token');
+
+//   if (!token) {
+//     redirect('/login');
+//   }
+
+  return <ManageEresources />;
+}
