e8ccd688593769bcdbf2e883d7f2805dfea59043
Prashanttheone1 <Prashant.Yadav@adeptek.co.in>
2026-01-05 23:37:24 +0530

    mbile edited

--- component/joinauthor/author.tsx
+++ component/joinauthor/author.tsx
@@ -242,9 +242,9 @@ export default function Author() {
                     px="16px"
                     mt="12px"
                   >
-                    <Text fontSize="sm" color="gray.400">
+                    {/* <Text fontSize="sm" color="gray.400">
                       Mobile
-                    </Text>
+                    </Text> */}
                     {/* <Text fontSize="md" color="#64B5F6" fontWeight="600">
                       9914583417 | 9816773505
                     </Text> */}
